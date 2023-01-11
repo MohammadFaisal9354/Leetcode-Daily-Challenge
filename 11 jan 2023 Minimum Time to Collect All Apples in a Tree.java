989c2e6cc3275a70913cb19c97ea1b1dcb4a6ab9
MohammadFaisal9354 <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-01-11 07:16:06 +0530

    Create 11 jan 2023 Minimum Time to Collect All Apples in a Tree.java

---
+++ 11 jan 2023 Minimum Time to Collect All Apples in a Tree.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {      
+        ArrayList<Integer>[] a = new ArrayList[n];
+        for(int i=0;i<n;i++){
+            a[i]=new ArrayList<Integer>();
+        }
+        for(int[] x:edges){
+            a[x[0]].add(x[1]);
+            a[x[1]].add(x[0]);
+        }
+
+        return dfs(-1,0,a,hasApple);
+    }
+
+    private int dfs(int prev,int curr,ArrayList<Integer>[] a,List<Boolean> hasApple)
+    {
+        int ans=0;
+        for(int x:a[curr])
+        {
+            if(x!=prev){
+               int res = dfs(curr,x,a,hasApple);
+               if(res>0||hasApple.get(x))
+                   ans+=(res+2);
+            }
+        }
+        return ans;
+    }
+}
