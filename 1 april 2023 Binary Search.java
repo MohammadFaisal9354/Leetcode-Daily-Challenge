7d35811ff5c8348e566e015bad220b3b06739e0d
MohammadFaisal9354 <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-04-01 09:43:04 +0530

    Create 1 april 2023 Binary Search.java

---
+++ 1 april 2023 Binary Search.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= target)
+                r = mid;
+            else
+                l = mid + 1;  
+        }
+
+        if (nums[l] != target) return -1;
+        return l;
+    }
+}
