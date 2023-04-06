3855f8ce02e9fc5b2b02c2cf389cc648ef1e0142
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-04-06 06:53:21 +0530

    Create 6 april 2023 Number of Closed Islands.java

---
+++ 6 april 2023 Number of Closed Islands.java
@@ -0,0 +1,35 @@
+class Solution {
+    private void dfs(int i, int j, int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 0) {
+            return;
+        }
+        grid[i][j] = 1;
+        dfs(i+1, j, grid);
+        dfs(i-1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i, j-1, grid);
+    }
+    
+    public int closedIsland(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            dfs(i, 0, grid);
+            dfs(i, n-1, grid);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(0, j, grid);
+            dfs(m-1, j, grid);
+        }
+        int count = 0;
+        for (int i = 1; i < m-1; i++) {
+            for (int j = 1; j < n-1; j++) {
+                if (grid[i][j] == 0) {
+                    dfs(i, j, grid);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
