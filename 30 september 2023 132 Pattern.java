94a08dfd31ae74fe8e6d337b5ef785ea5f3da482
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-10-01 06:12:14 +0530

    Create 30 september 2023 132 Pattern.java

---
+++ 30 september 2023 132 Pattern.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int max3 = Integer.MIN_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < max3)
+                return true;
+            while (!stack.isEmpty() && stack.peek() < nums[i]) {
+                max3 = stack.peek();
+                stack.pop();
+            }
+            stack.push(nums[i]);
+        }
+        return false;
+    }
+}
