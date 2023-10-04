badf6dac232ae374073e637e5b94194342d0fa2c
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-10-04 12:56:55 +0530

    Create 4 october 2023 Design HashMap.java

---
+++ 4 october 2023 Design HashMap.java
@@ -0,0 +1,18 @@
+class MyHashMap {
+    private static int[] map = new int[1000001];
+    public MyHashMap() {
+        Arrays.fill(map, -1);
+    }
+    
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    public int get(int key) {
+        return  map[key];
+    }
+    
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
