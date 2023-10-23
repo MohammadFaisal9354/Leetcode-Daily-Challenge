f1763e33102449f0ae1255c7fef1e61c3f6cf63f
Mohammad Faisal <faisalkhan9354@gmail.com>
2023-10-23 17:42:58 +0530

    Create 23 october 2023 Power of Four.java

---
+++ 23 october 2023 Power of Four.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {        
+        while(n>1){
+            if(n%4!=0) return false;
+            n=n/4;
+        }
+        if(n==1) return true;
+        return false;
+    }
+}
