17b32194c72c37c2f99d9386881714d1c441da66
Mohammad Faisal <faisalkhan9354@gmail.com>
2024-01-18 08:29:03 +0530

    Create 18 January 2024 Climbing Stairs.java

---
+++ 18 January 2024 Climbing Stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+      if(n<=2)
+        return n;
+        int arr[] = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;        
+        for(int i=3;i<arr.length;i++)
+            arr[i] = arr[i-1] + arr[i-2]; 
+        return arr[n];  
+        }
+}
