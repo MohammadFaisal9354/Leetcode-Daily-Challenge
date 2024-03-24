6c7e203b79a4b8644bff3b750faef49397f93038
Mohammad Faisal <faisalkhan9354@gmail.com>
2024-03-24 21:02:33 +0530

    Create 24 March 2024 Find the Duplicate Number.java

---
+++ 24 March 2024 Find the Duplicate Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(!set.add(num)) {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
